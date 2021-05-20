d1d3739cf06dd52774d8032d17cc336ad567e695
OmaimaSalem <omaimasalem933@gmail.com>
2021-05-20 21:55:42 +0200

    first commit

---
+++ app/invoices.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class invoices extends Model
+{
+    use SoftDeletes;
+    protected $guarded = [];
+    public function section()
+    {
+    return $this->belongsTo('App\sections');
+    }
+}
